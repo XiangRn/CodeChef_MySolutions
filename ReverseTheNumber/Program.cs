68cd0e88ecc98cffe10a612e2c49ddc1ffe67d01
XiangRn <cuadao1710@gmail.com>
2021-05-19 23:00:18 +0700

    My first solution

---
+++ ReverseTheNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[t];
+            for (int i = 0; i < t; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(string.Join("", numbers[i].ToString().Reverse().ToArray()));
+            }
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
